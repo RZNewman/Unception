1fc893cbbc4a8812b899c76d64f34d766c3e6135
RZNewman <ruznewman@gmail.com>
2022-06-10 12:27:01 -0500

    attack size with player height as min

--- Assets/Attack/InstantHit.cs
+++ Assets/Attack/InstantHit.cs
@@ -6,30 +6,32 @@ public static class InstantHit
 {
     public static List<GameObject> LineAttack(Transform body,float raduis, float halfHeight, float length, float width)
     {
-        float totalHeight = halfHeight * 2 * 1.5f;
+        float playerHeightOversize = halfHeight * 2 * 1.5f;
         List<GameObject> hits = new List<GameObject>();
         List<GameObject> tempHits = new List<GameObject>();
         Vector3 bodyFocus = body.position + body.forward * raduis;
         Vector2 attackVec = new Vector2(length, width/2);
         float maxDistance = attackVec.magnitude;
         Vector3 boxCenter = bodyFocus + maxDistance * 0.5f * body.forward;
-        Vector3 boxHalfs = new Vector3(width / 2, totalHeight/2, maxDistance / 2);
+        float boxHeight = Mathf.Max(playerHeightOversize, maxDistance);
+        Vector3 boxHalfs = new Vector3(width / 2, boxHeight / 2, maxDistance / 2);
 
         Quaternion q = Quaternion.LookRotation(body.forward);
         RaycastHit[] boxHits = Physics.BoxCastAll(boxCenter, boxHalfs, body.forward, q, 0.0f, LayerMask.GetMask("Players"));
         //RaycastHit[] sphereHits = Physics.SphereCastAll(bodyFocus, maxDistance, body.forward, 0.0f, LayerMask.GetMask("Players"));
-        Vector3 capsuleHeightDiff = body.up * totalHeight / 2; //height multiplier
+        float capsuleHeightFactor = Mathf.Max(boxHeight / 2 - maxDistance, 0);
+        Vector3 capsuleHeightDiff = body.up * capsuleHeightFactor; 
         Vector3 capsuleStart = bodyFocus + capsuleHeightDiff;
         Vector3 capsuleEnd = bodyFocus - capsuleHeightDiff;
         RaycastHit[] capsuleHits = Physics.CapsuleCastAll(capsuleStart, capsuleEnd, maxDistance, body.forward, 0.0f, LayerMask.GetMask("Players"));
 
         //Debug.DrawLine(bodyFocus, bodyFocus + body.forward * maxDistance, Color.blue, 3.0f); ;
         //Debug.DrawLine(bodyFocus, bodyFocus + (body.forward+body.up).normalized * maxDistance, Color.blue, 3.0f);
-        //DrawBox(boxCenter, q,boxHalfs*2, Color.blue);
-        //Debug.DrawLine(capsuleStart, capsuleEnd, Color.red);
-        //Debug.DrawLine(capsuleStart, capsuleStart+ body.forward*maxDistance, Color.red);
-        //Debug.DrawLine(capsuleEnd, capsuleEnd + body.forward * maxDistance, Color.red);
-        //Debug.Break();
+        DrawBox(boxCenter, q,boxHalfs*2, Color.blue);
+        Debug.DrawLine(capsuleStart, capsuleEnd, Color.red);
+        Debug.DrawLine(capsuleStart, capsuleStart+ body.forward*maxDistance, Color.red);
+        Debug.DrawLine(capsuleEnd, capsuleEnd + body.forward * maxDistance, Color.red);
+        Debug.Break();
 
         foreach (RaycastHit hit in boxHits)
         {

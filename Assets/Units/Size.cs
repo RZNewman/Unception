cd9ff596454f41dd628c1f0dd3a41a8724895b83
RZNewman <father.anger@gmail.com>
2023-12-29 14:18:07 -0500

    Remove STS

--- Assets/Units/Size.cs
+++ Assets/Units/Size.cs
@@ -1,4 +1,3 @@
-using ShaderCrew.SeeThroughShader;
 using UnityEngine;
 using static IndicatorHolder;
 
@@ -55,12 +54,6 @@ public class Size : MonoBehaviour
         stopper.transform.localScale = Vector3.one;
         stopper.radius = colliderWidth + 0.01f;
         stopper.height = colliderHalfHeight * 2 + 0.01f;
-
-        if (GetComponentInParent<UnitPropsHolder>().props.isPlayer)
-        {
-            FindObjectOfType<PlayersPositionManager>().AddPlayerAtRuntime(stopper.gameObject);
-            stopper.GetComponent<SeeThroughShaderPlayer>().enabled = true;
-        }
         GetComponentInParent<Power>().subscribePower(updateSize);
     }
 
